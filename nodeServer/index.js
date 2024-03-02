575291721928b824e9c98c7297fa5d824fb00786
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-03-02 21:21:49 +0530

    Third commit

---
+++ nodeServer/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, ()=> console.log("Server On PORT ${PORT}"));
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
